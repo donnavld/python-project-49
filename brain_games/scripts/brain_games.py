c77002e1d1f6f44c2fa072e8839922204bedce70
Elena <donna_vld@mail.ru>
2024-08-25 11:29:13 -0400

    3 step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,12 @@
+#from brain_games.scripts.game_calculator import run_calculator_game
+#from brain_games.scripts.game_progression import run_progression_game
+#from brain_games.scripts.game_even import run_even_number_game
+#from brain_games.scripts.game_greatest_factor import run_greatest_factor_game
+#from brain_games.scripts.game_prime import run_prime_number_game
+from brain_games.cli import welcome_user
+
 def main():
-    print(' Welcome to the Brain Games!')
+    name = welcome_user()
 
 
 if __name__ == '__main__':
