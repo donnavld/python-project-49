62e1554e3ea8f460b96d2ee93c3871ce942daf01
Elena <donna_vld@mail.ru>
2024-08-27 18:50:25 -0400

    correct games

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -10,26 +10,30 @@ def get_greatest_factor(num1, num2):
 
 def main():
     name = welcome_user()
-    print(f'Welcome to The Greatest Common Factor game, {name}!\n')    
-
+    #print(f'Welcome to The Greatest Common Factor game, {name}!\n')    
+    print('Find the greatest common divisor of given numbers.')
     user_try = 0
     while user_try < 3:
         num1 = random.randint(0, 100)
         num2 = random.randint(0, 100)
         greatest_factor = get_greatest_factor(num1, num2)
         #print (greatest_factor)
-        user_answer = input(f'Enter the  Greatest Common Factor for {num1} and {num2} : ')
-        while not user_answer.isdigit():
-            user_answer = input('Please enter just number:')
+        #user_answer = input(f'Enter the  Greatest Common Factor for {num1} and {num2} : ')
+        user_answer = input(f'Question: {num1} {num2}: \nYour answer: ')
+        #while not user_answer.isdigit():
+            #user_answer = input('Please enter just number:')
         if greatest_factor == int(user_answer):
-            print("Correct!\n")
+            print("Correct!")
             user_try += 1
         else:
-            print("Incorrect.\n")
+            #print("Incorrect.\n")
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{greatest_factor}'.")
             break
 
     if user_try == 3:
-        print('Congratulations! You won!')
+        #print('Congratulations! You won!')
+        print(f'Congratulations, {name}!')
     else:
-        print('You lose.')
+        #print('You lose.')
+        print(f"Let's try again, {name}!")
 
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -4,32 +4,37 @@ from brain_games.cli import welcome_user
 
 def main():
     name = welcome_user()
-    print(f'Welcome to The Progression game, {name}!\n')
+    #print(f'Welcome to The Progression game, {name}!\n')
+    print('What number is missing in the progression?')
 
     user_try = 0
     while user_try < 3:
         step = random.randint(0, 10)
         start = random.randint(0, 10)
-        length = 5
+        length = 10
         progression = [start + step * i for i in range(length)]
         missing_number = random.choice(progression)
         index = progression.index(missing_number)
-        progression[index] = ' ... '
-        print('Look at a progression')
-        print(progression)
+        progression[index] = ' .. '
+        #print('Look at a progression')
+        #print(progression)
         #print(missing_number)
-        user_number = input('Enter the missing : ')
-        while not user_number.isdigit():
-            user_number = input('Please enter just numbers:')
+        #user_number = input('Enter the missing : ')
+        user_number = input(f'Question: {progression} \nYour answer: ')
+        #while not user_number.isdigit():
+            #user_number = input('Please enter just numbers:')
         if int(user_number) == int(missing_number):
-            print("Correct!\n")
+            print("Correct!")
             user_try += 1
         else:
-            print("Incorrect.\n")
+            #print("Incorrect.\n")
+            print(f"'{user_number}' is wrong answer ;(. Correct answer was '{missing_number}'.")
             break
 
     if user_try == 3:
-        print('Congratulations! You won!')
+        #print('Congratulations! You won!')
+        print(f'Congratulations, {name}!')
     else:
-        print('You lose.')
+        #print('You lose.')
+        print(f"Let's try again, {name}!")
 
