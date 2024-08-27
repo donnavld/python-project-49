23296544155f1c2eb8c1f799a3b1507e18cd9b50
Elena <donna_vld@mail.ru>
2024-08-27 18:05:43 -0400

    correct calculator game

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -6,8 +6,8 @@ from brain_games.cli import welcome_user
 
 def main():
     name = welcome_user()
-    print(f'Welcome to the Calculator game, {name}!\n')
-
+    #print(f'Welcome to the Calculator game, {name}!\n')
+    print('What is the result of the expression?')
     operations = {
         '+': operator.add,
         '-': operator.sub,
@@ -20,17 +20,20 @@ def main():
         num1 = random.randint(0, 10)
         num2 = random.randint(0, 10)
         result = operations[operation](num1, num2)
-        user_result = input(f'Please calculate: {num1} {operation} {num2} = ')
-        while not used_simbols.match(user_result):
-            user_result = input('Please enter just numbers:')
+        user_result = input(f'Question:: {num1} {operation} {num2} \nYour answer: ')
+        #while not used_simbols.match(user_result):
+            #user_result = input('Please enter just numbers:')
         if result == int(user_result):
             print("Correct!")
             user_try += 1
         else:
-            print("Incorrect.")
+            #print("Incorrect.")
+            print(f"'{user_result}' is wrong answer ;(. Correct answer was '{result}'.")
             break
     if user_try == 3:
-        print('Congratulations! You won!')
+        #print('Congratulations! You won!')
+        print(f'Congratulations, {name}!')
     else:
-        print('You lose.')
+        #print('You lose.')
+        print(f"Let's try again, {name}!")
 
